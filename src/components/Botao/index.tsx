b9fef0097f9487b5eded4a1634fa0940a178b240
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-10-16 21:23:36 -0300

    botao estilizado

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './style.scss'
 
 class Botao extends React.Component{
   render() {
+    
     return (
-      <button>
+      <button className="botao">
         Botão
       </button>
     )
