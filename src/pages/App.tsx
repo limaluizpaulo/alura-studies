5a477065e06affeddbda7e562a0a07a534193889
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-10-16 21:23:24 -0300

    mudança de diretorio do App

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../components/Fomulario'
+import Lista from '../components/Lista'
+import './style.scss'
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario/>
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
