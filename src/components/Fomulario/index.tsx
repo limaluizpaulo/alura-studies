766c07d6d9683a6b4b1759675dc5ba4c2099276e
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-10-16 21:23:58 -0300

    Formulario estilizado

--- src/components/Fomulario/index.tsx
+++ src/components/Fomulario/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Botao from '../Botao'
+import './style.scss'
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form >
-        <div>
+      <form className="novaTarefa" >
+        <div className="inputContainer">
         <label htmlFor="tarefa">Adicione um novo estilo</label>
         <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required/>
         </div>
-        <div>
+        <div className="inputContainer">
           <label htmlFor="tempo">
             Tempo
         <input type="time" name="tempo" id="tempo" min="00:00:00" max="01:30:00" step="1" placeholder="Tempo" required/>
